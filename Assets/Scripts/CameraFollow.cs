37bd909eaf40d11234e6b4e7c6a5b7254125d8d6
Miiru <36801659+Miiruxd@users.noreply.github.com>
2021-02-03 16:45:57 +0200

    Limit camera movement to room boundaries

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -24,7 +24,16 @@ public class CameraFollow : MonoBehaviour
    {
 		if (playerTransform != null)
 		{
-			transform.position = playerTransform.position + new Vector3(0,10,-10);
+			transform.position = playerTransform.position + new Vector3(0,35,-10);
+
+			if (transform.position.x < -115)
+				transform.SetPositionAndRotation(new Vector3(-115, transform.position.y, transform.position.z), transform.rotation);
+
+			if (transform.position.x > 45)
+				transform.SetPositionAndRotation(new Vector3(45, transform.position.y, transform.position.z), transform.rotation);
+
+			if (transform.position.y > 32)
+				transform.SetPositionAndRotation(new Vector3(transform.position.x, 32, transform.position.z), transform.rotation);
 		}
    }
 }
