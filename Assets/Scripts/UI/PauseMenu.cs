774703bafab23915d64c95c7660865a533982f22
NobodyCreator <45387429+NobodyCreator@users.noreply.github.com>
2021-02-04 15:10:45 +0200

    this is it this is the pause menu yes lets go finally haha hehe
    look it's basic  i know but you can press "p" for pause nothing else and then you have option of 3 (three) continue or resume in english to resume the game (that is being played), main menu option to go to new level called main menu where you can go play game called main menu and third option is to rage quit by pressing quit button and leaving game forever until next time you boot up the game and choose the same option

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool GameIsPaused = false;
+
+
+    public GameObject pauseMenuUi;
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+
+            }
+        }
+    }
+
+    public void Resume ()
+    {
+        pauseMenuUi.SetActive(false);
+        GameIsPaused = false;
+    }
+
+    void Pause ()
+
+    {
+        pauseMenuUi.SetActive(true);
+        GameIsPaused = true;
+        
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
