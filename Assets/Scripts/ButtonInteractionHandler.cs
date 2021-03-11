077571c9c116c564056d332ed69b9688e6b176c5
Miiru <36801659+Miiruxd@users.noreply.github.com>
2021-03-11 22:33:43 +0200

    Implemented Ava's button functionality @level 1
    - The hatch no longer opens when Van interacts with the lever to free Ava. Ava has to interact with a button on the wall (Van can not reach that point) in order to open the hatch so both of them can escape
    - Also modified fullscreen to window mode because it was annoying, feel free to change it back if you want
    - Next on to do: highlight the objects that each player can interact with (make the game more user friendly)

---
+++ Assets/Scripts/ButtonInteractionHandler.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+public class ButtonInteractionHandler : MonoBehaviour
+{
+    public GameObject buttonNotPressed;
+    public GameObject buttonPressed;
+    public GameObject hatchOpen;
+    public GameObject hatchClosed;
+    public GameObject LevelTravelBox;
+
+    PhotonView photonView;
+
+    bool interactable = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        interactable = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        interactable = false;
+    }
+
+    void Start()
+    {
+        photonView = gameObject.GetComponent<PhotonView>();
+    }
+
+    void Update()
+    {
+        if (interactable)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                photonView.RPC("openHatch", RpcTarget.All);
+            }
+        }
+    }
+
+    [PunRPC]
+    void openHatch()
+    {
+        buttonNotPressed.SetActive(false);
+        buttonPressed.SetActive(true);
+
+        hatchClosed.SetActive(false);
+        hatchOpen.SetActive(true);
+
+        LevelTravelBox.SetActive(true);
+
+        interactable = false;
+    }
+}
