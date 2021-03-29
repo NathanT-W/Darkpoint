bf2695c81e7d6d5c57a476402c445ddf0ebbbf62
Miiru <36801659+Miiruxd@users.noreply.github.com>
2021-03-29 22:02:13 +0300

    Started working on the bird mechanics (WIP)

---
+++ Assets/Scripts/BirdAI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class BirdAI : MonoBehaviour
+{
+    public int speed = 8;
+
+    bool moveTowardsRight = true;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "TurningPoint")
+            moveTowardsRight = !moveTowardsRight;
+    }
+
+    void Update()
+    {
+        transform.Translate((moveTowardsRight ? 2 : -2) * Time.deltaTime * speed, 0, 0);
+    }
+}
