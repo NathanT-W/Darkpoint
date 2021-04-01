1979b98772553d8fc7313a568bc758da69c36e57
Xenohell <vodnarana@yahoo.com>
2021-04-01 17:29:22 +0300

    Level travel fixed
    I fixed the level travel issues. I promise, I fixed it. It works magically. If you manage to somehow break it, you redo it, cause it broke because of you.
    But no, seriously, the travel is fixed now.

---
+++ Assets/Scripts/LevelTravel1.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class LevelTravel1 : MonoBehaviour
+{
+    private bool VanReadyToLeave;
+    private bool AvaReadyToLeave;
+
+    private GameObject Van, Ava;
+
+    public GameObject VanSpawnPoint2, AvaSpawnPoint2;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            VanReadyToLeave = true;
+        if (collision.gameObject.tag == "Fairy")
+            AvaReadyToLeave = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            VanReadyToLeave = false;
+        if (collision.gameObject.tag == "Fairy")
+            AvaReadyToLeave = false;
+    }
+
+    void Update()
+    {
+        Van = GameObject.FindGameObjectWithTag("Player");
+        Ava = GameObject.FindGameObjectWithTag("Fairy");
+
+        if (VanReadyToLeave && AvaReadyToLeave && GameManager.currentLevel == 1 && this.name == "Level1TravelBox")
+            TeleportLevel2();
+            //StartCoroutine(TeleportLevel2());
+    }
+
+    void TeleportLevel2()
+    {
+        VanReadyToLeave = false;
+        AvaReadyToLeave = false;
+
+        //yield return new WaitForSeconds(1);
+
+        Van.transform.position = VanSpawnPoint2.transform.position;
+        Ava.transform.position = AvaSpawnPoint2.transform.position;
+
+        GameManager.currentLevel = 2;
+
+        Van.SetActive(false);
+        Ava.SetActive(false);
+
+        Camera.main.GetComponent<CameraFollow>().enabled = false;
+        Camera.main.GetComponent<CutsceneEndScript>().secondLevelCutscenePlay();
+
+        //StopCoroutine(TeleportLevel2());
+    }
+}
