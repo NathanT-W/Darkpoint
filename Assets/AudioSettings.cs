8562f957de0fe4a4f31c29ba02bb947bc6ba4eef
NobodyCreator <45387429+NobodyCreator@users.noreply.github.com>
2021-04-29 16:45:59 +0300

    check it out music changes nice or no?

---
+++ Assets/AudioSettings.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSettings : MonoBehaviour
+{
+
+    private static readonly string BackgroundPref = "BackgroundPref";
+    private static readonly string SoundEffectsPref = "SoundEffectsPref";
+    private float backgroundfloat, soundEffectsfloat;
+    public AudioSource backgroundAudio;
+    public AudioSource[] soundEffectsAudio;
+
+    void Awake()
+    {
+        ContinueSettings();
+    }
+
+    
+    private void ContinueSettings()
+    {
+        backgroundfloat = PlayerPrefs.GetFloat(BackgroundPref);
+        soundEffectsfloat = PlayerPrefs.GetFloat(SoundEffectsPref);
+
+        backgroundAudio.volume = backgroundfloat;
+
+        for (int i = 0; i < soundEffectsAudio.Length; i++)
+        {
+            soundEffectsAudio[i].volume = soundEffectsfloat;
+
+
+        }
+    }
+}
