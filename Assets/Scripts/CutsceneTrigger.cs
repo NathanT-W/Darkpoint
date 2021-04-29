fb768470586687292e7ded3af89b78695148e70f
NathanT-W <nathtw1@gmail.com>
2021-04-29 14:06:05 +0100

    Panic work trying to put level 3 in the main level and make transition, not currently working properly.

---
+++ Assets/Scripts/CutsceneTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CutsceneTrigger : MonoBehaviour
+{
+    public PlayableDirector sceneToPlay;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Camera.main.GetComponent<CameraFollow>().enabled = false;
+        sceneToPlay.Play();
+
+        GameObject.FindGameObjectWithTag("Player").SetActive(false);
+        GameObject.FindGameObjectWithTag("Fairy").SetActive(false);
+    }
+}
